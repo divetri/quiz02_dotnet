486d63061a2c4cce537e3be3269ecc27dcfeb846
Divetri Ayu Rahmawati <54519415+divetri@users.noreply.github.com>
2022-03-16 21:56:12 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr1234 = new int[] {15, 2, 30, 12, 10, 5};
+int[] arr5 = new int[] {5, 2, 30, 12, 10, 5, 2, 10, 5, 30, 45};
+int[] arr6 = new int[] { 1, 5, 3, 7, 8, 5, 1 };
+Quiz02.Solution.Soal1(arr1234);
+Console.WriteLine("------------------------");
+Quiz02.Solution.Soal2(arr1234);
+Console.WriteLine("------------------------");
+Quiz02.Solution.Soal3(arr1234);
+Console.WriteLine("------------------------");
+Quiz02.Solution.Soal4(arr1234);
+Console.WriteLine("------------------------");
+Quiz02.Solution.Soal5(arr5);
+Console.WriteLine("------------------------");
+Quiz02.Solution.Soal6(arr6);
+Console.ReadLine();
---
+++ Solution.cs
@@ -0,0 +1,135 @@
+﻿namespace Quiz02
+{
+    class Solution
+    {
+        public static void ReadInt(int[] list)
+        {
+            for(int i=0; i<list.Length; i++)
+            {
+                Console.Write($"{list[i]} ");
+                
+            }
+            Console.WriteLine();
+        }
+        public static int[] leftShiftInt(int[] list)
+        {
+            int[] shiftedArray = new int[list.Length];
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (i == list.Length - 1)
+                {
+                    shiftedArray[i] = list[0];
+                }
+                else
+                {
+                    shiftedArray[i] = list[i + 1];
+                }
+            }
+            return shiftedArray;
+        }
+        public static int[] rightShiftInt(int[] list)
+        {
+            int[] shiftedArray = new int[list.Length];
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (i == 0)
+                {
+                    shiftedArray[i] = list[list.Length-1];
+                }
+                else
+                {
+                    shiftedArray[i] = list[i-1];
+                }
+            }
+            return shiftedArray;
+        }
+        public static void Soal1(int[] list)
+        {
+            Console.WriteLine("Before Random");
+            ReadInt(list);
+            Console.WriteLine("After Random Position Element Change");
+            Random random = new Random();
+            int[] randomArray = new int[list.Length];
+            for (int i = 0; i < list.Length; i++)
+            {
+                randomArray[i] = list[random.Next(0, list.Length-1)];
+            }
+            ReadInt(randomArray);
+        }
+        public static void Soal2(int[] list)
+        {
+            Console.WriteLine("Before Shifting");
+            ReadInt(list);
+            Console.WriteLine("After Shifting");
+            ReadInt(leftShiftInt(list));
+        }
+        public static void Soal3(int[] list)
+        {
+            Console.WriteLine("Rotate Shifting 3x");
+            ReadInt(list);
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Rotate ke {i}");
+                ReadInt(leftShiftInt(list));
+                list = leftShiftInt(list);                
+            }  
+        }
+        public static void Soal4(int[] list) {
+            Console.WriteLine("Rotate Shifting 3x");
+            ReadInt(list);
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Rotate ke {i}");
+                ReadInt(rightShiftInt(list));
+                list = rightShiftInt(list);
+            }
+        }
+        public static void Soal5(int[] list) {
+            ReadInt(list);
+            Array.Sort(list);
+            int count = 1;
+            for (int i=0; i<list.Length; i++)
+            {
+                int x = list[i];
+                if (i<list.Length-1 && x == list[i + 1])
+                {
+                    count++;
+                }
+                else if (i == list.Length-1 && x==list[i-1])
+                {
+                    Console.WriteLine($"{list[i]} muncul {count} kali");
+                }
+                else {
+                    Console.WriteLine($"{list[i]} muncul {count} kali");
+                    count = 1;
+                }
+            }
+        }
+        public static void Soal6(int[] list)
+        {
+            Console.WriteLine("Before Marker");
+            ReadInt(list);
+            Array.Sort(list);
+            for (int i=0; i<list.Length; i++)
+            {
+                int x = list[i];
+                if(i<list.Length-1 && x == list[i + 1])
+                {
+                    for(int j=i; j<list.Length; j++)
+                    {
+                        if (j == list.Length - 1)
+                        {
+                            list[j] = -1;
+                        }
+                        else
+                        {
+                            list[j] = list[j + 1];
+                        }
+                        
+                    }
+                }
+            }
+            ReadInt(list);
+        }
+    }
+}
